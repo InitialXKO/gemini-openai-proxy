c4511619c7666220850fedff6388191510759180
InitialXKO <45725592+InitialXKO@users.noreply.github.com>
2023-12-19 17:12:40 +0800

    Update deploy.sh

--- .container-hosting/deploy.sh
+++ .container-hosting/deploy.sh
@@ -148,8 +148,8 @@ ssh-add -l
 SSH_ARGS="-F $SSH_CONFIG_FILE"
 
 amber exec -- sh -c ''"ssh $SSH_ARGS"' dokku@$DOKKU_HOST -C $CONTAINER_HOSTING_API_KEY dokku apps:create $APP_NAME || true'
-amber exec -- sh -c ''"ssh $SSH_ARGS"' dokku@$DOKKU_HOST -C $CONTAINER_HOSTING_API_KEY dokku builder:set $APP_NAME build-dir /'
-amber exec -- sh -c ''"ssh $SSH_ARGS"' dokku@$DOKKU_HOST -C $CONTAINER_HOSTING_API_KEY dokku builder-dockerfile:set $APP_NAME dockerfile-path Dockerfile'
+# amber exec -- sh -c ''"ssh $SSH_ARGS"' dokku@$DOKKU_HOST -C $CONTAINER_HOSTING_API_KEY dokku builder:set $APP_NAME build-dir /'
+# amber exec -- sh -c ''"ssh $SSH_ARGS"' dokku@$DOKKU_HOST -C $CONTAINER_HOSTING_API_KEY dokku builder-dockerfile:set $APP_NAME dockerfile-path Dockerfile'
 
 # Set common env settings
 
